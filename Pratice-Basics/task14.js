1dd416387c80a47bc7b12d4ce0388f10b1cb28b0
Sunil Gautam <gautamsunil917@gmail.com>
2026-02-10 23:51:44 +0545

    task14 done

--- Pratice-Basics/task14.js
+++ Pratice-Basics/task14.js
@@ -1,3 +1,15 @@
-function task14() {
-    
+function task14(number) {
+  let oddCount = 0;
+  let evenCount = 0;
+
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 == 0) {
+      evenCount = evenCount + 1;
+    } else {
+      oddCount = oddCount + 1;
+    }
+  }
+  return {"odd" : oddCount, "even": evenCount};
 }
+
+console.log(task14(10))
